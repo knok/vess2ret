a9cff6169b6f89c044b16894ab4c229a27e4d84f
NOKUBI Takatsugu <knok@daionet.gr.jp>
2017-09-05 13:18:46 +0900

    regularize image array

--- util/util.py
+++ util/util.py
@@ -34,30 +34,30 @@ class MyDict(dict):
 
 def convert_to_rgb(img, is_binary=False):
     """Given an image, make sure it has 3 channels and that it is between 0 and 1."""
-    if len(img.shape) != 3:
-        raise Exception("""Image must have 3 dimensions (channels x height x width). """
-                        """Given {0}""".format(len(img.shape)))
+    # if len(img.shape) != 3:
+    #     raise Exception("""Image must have 3 dimensions (channels x height x width). """
+    #                     """Given {0}""".format(len(img.shape)))
 
-    img_ch, _, _ = img.shape
+    _, _, img_ch = img.shape
     if img_ch != 3 and img_ch != 1:
         raise Exception("""Unsupported number of channels. """
                         """Must be 1 or 3, given {0}.""".format(img_ch))
 
     imgp = img
     if img_ch == 1:
-        imgp = np.repeat(img, 3, axis=0)
+        imgp = np.repeat(img, 3, axis=-1)
 
     if not is_binary:
         imgp = imgp * 127.5 + 127.5
         imgp /= 255.
 
-    return np.clip(imgp.transpose((1, 2, 0)), 0, 1)
+    return np.clip(imgp, 0, 1)
 
 
 def compose_imgs(a, b, is_a_binary=True, is_b_binary=False):
     """Place a and b side by side to be plotted."""
-    #ap = convert_to_rgb(a, is_binary=is_a_binary)
-    #bp = convert_to_rgb(b, is_binary=is_b_binary)
+    ap = convert_to_rgb(a, is_binary=is_a_binary)
+    bp = convert_to_rgb(b, is_binary=is_b_binary)
 
     if ap.shape != bp.shape:
         raise Exception("""A and B must have the same size. """
